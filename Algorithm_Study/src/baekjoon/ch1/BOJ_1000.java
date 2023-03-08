ed8cbcb8a47cf43571c2ba749b334928cb433806
NegotiationMan <dwc07238@naver.com>
2023-03-08 21:35:04 +0900

    :sparkles: 입출력과 사칙연산 풀이

---
+++ Algorithm_Study/src/baekjoon/ch1/BOJ_1000.java
@@ -0,0 +1,15 @@
+package baekjoon.ch1;
+
+import java.util.Scanner;
+
+// A + B
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // a, b 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
---
+++ Algorithm_Study/src/baekjoon/ch1/BOJ_1008.java
@@ -0,0 +1,15 @@
+package baekjoon.ch1;
+
+import java.util.Scanner;
+
+// A / B
+public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // a, b 입력
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.println(a / b);
+    }
+}

e95ee0794326e70cf5ca079eed6f4e04627ac54a
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-09-18 15:50:06 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.py
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.py
@@ -1,6 +1,5 @@
 import sys
 
 for _ in range(int(input())):
-  s = sys.stdin.readline().rstrip()
-  print("yes" if len(s)>=6 and len(s)<=9 else "no")
-  
+    s = sys.stdin.readline().rstrip()
+    print("yes" if 6 <= len(s) <= 9 else "no")

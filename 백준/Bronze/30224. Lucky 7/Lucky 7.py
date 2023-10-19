94c7799ab69109a7c55b78b0098ac3e07bf878f4
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-10-19 23:26:12 +0900

    [Bronze V] Title: Lucky 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30224. Lucky 7/Lucky 7.py
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.py
@@ -1,10 +1,12 @@
 n = input()
 
-if '7' in n and int(n) % 7 != 0:
-    print(2)
-elif '7' in n and int(n) % 7 == 0:
-    print(3)
-elif '7' not in n and int(n) % 7 == 0:
-    print(1)
-elif '7' not in n and int(n) % 7 != 0:
-    print(0)
+if '7' in n:
+    if int(n) % 7 == 0:
+        print(3)
+    else:
+        print(2)
+else:
+    if int(n) % 7 == 0:
+        print(1)
+    else:
+        print(0)

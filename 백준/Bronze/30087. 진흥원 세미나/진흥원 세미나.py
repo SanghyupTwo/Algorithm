c949c39497c560db42d7b3de5151d732f473e940
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-10-19 23:35:30 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+  s = input()
+  if s == "Algorithm":
+    print(204)
+  elif s == "DataAnalysis":
+    print(207)
+  elif s == "ArtificialIntelligence":
+    print(302)
+  elif s == "CyberSecurity":
+    print("B101")
+  elif s == "Network":
+   print(303)
+  elif s == "Startup":
+    print(501)
+  elif s == "TestStrategy":
+    print(105)

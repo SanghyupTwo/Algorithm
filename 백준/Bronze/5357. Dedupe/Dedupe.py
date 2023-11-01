3ffe5f39c12a3f6191692da308cb737b16f4851d
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-11-01 16:59:46 +0900

    [Bronze IV] Title: Dedupe, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5357. Dedupe/Dedupe.py
+++ 백준/Bronze/5357. Dedupe/Dedupe.py
@@ -1,11 +1,7 @@
 for _ in range(int(input())):
   s = input()
   res = s[0]
-  for i in range(len(s)):
-    if i == 0:
-      continue
-    if s[i] == s[i-1]:
-      continue
-    else:
+  for i in range(1, len(s)):
+    if s[i] != s[i-1]:
       res += s[i]
   print(res)

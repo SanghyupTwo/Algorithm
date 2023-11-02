d9f90595d319c1d73ad117bc871ff89ddf65430f
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-11-02 15:33:57 +0900

    [Bronze IV] Title: 타임 카드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5575. 타임 카드/타임 카드.py
+++ 백준/Bronze/5575. 타임 카드/타임 카드.py
@@ -1,20 +1,11 @@
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-c = list(map(int, input().split()))
+for _ in range(3):
+    h1, m1, s1, h2, m2, s2 = map(int, input().split())
+    t1 = h1 * 60 * 60 + m1 * 60 + s1
+    t2 = h2 * 60 * 60 + m2 * 60 + s2
+    res = t2 - t1
 
-res1 = [a[3 + i] - a[i] for i in range(3)]
-res2 = [b[3 + i] - b[i] for i in range(3)]
-res3 = [c[3 + i] - c[i] for i in range(3)]
+    h = res // 60 // 60 % 24
+    m = res // 60 % 60
+    s = res % 60
 
-for i in range(2, -1, -1):
-    if res1[i] < 0:
-        res1[i] += 60
-        res1[i - 1] -= 1
-    if res2[i] < 0:
-        res2[i] += 60
-        res2[i - 1] -= 1
-    if res3[i] < 0:
-        res3[i] += 60
-        res3[i - 1] -= 1
-
-print(' '.join(map(str, res1)), ' '.join(map(str, res2)), ' '.join(map(str, res3)), sep='\n')
+    print(h, m, s)

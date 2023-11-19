c8f65722e163d22b3ab3e3eacbcf13d6d04704fa
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-11-19 23:24:00 +0900

    [Bronze V] Title: Loteria Falha, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30664. Loteria Falha/Loteria Falha.py
+++ 백준/Bronze/30664. Loteria Falha/Loteria Falha.py
@@ -2,7 +2,4 @@ while True:
     n = int(input())
     if n == 0:
         break
-    if n % 42 == 0:
-        print("PREMIADO")
-    else:
-        print("TENTE NOVAMENTE")
+    print("PREMIADO" if n % 42 == 0 else "TENTE NOVAMENTE")

2b4af56dc54aec4c88adb6d5cf471c33ee551356
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2023-12-31 19:43:36 +0900

    [Bronze IV] Title: Amusement Park Adventure, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29986. Amusement Park Adventure/Amusement Park Adventure.py
+++ 백준/Bronze/29986. Amusement Park Adventure/Amusement Park Adventure.py
@@ -1,9 +1,3 @@
 n, h = map(int, input().split())
 arr = list(map(int, input().split()))
-res = 0
-
-for i in arr:
-    if h >= i:
-        res += 1
-        
-print(res)
+print(sum(1 for i in arr if h >= i))

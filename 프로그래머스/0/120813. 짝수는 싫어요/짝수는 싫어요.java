61f648eee6420a6910f1d1ba0d4043d3b7aa2f46
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-05-21 20:54:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.88 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -6,13 +6,6 @@ class Solution {
         for (int i = 1; i <= n; i += 2) {
             list.add(i);
         }
-
-        // ArrayList<Integer>를 int[]로 변환
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
-
-        return result;
+        return list.stream().mapToInt(x -> x).toArray();
     }
 }

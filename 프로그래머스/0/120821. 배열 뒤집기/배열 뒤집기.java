07d628252884b0b97f4249f099cbab28e257081f
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-05-25 17:25:36 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.90 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] result = new int[num_list.length];
-        int cnt = 0;
-        for (int i = num_list.length - 1; i >= 0; i--) {
-            result[cnt] = num_list[i];
-            cnt ++;
-        }
-        return result;
+        return IntStream.rangeClosed(1, num_list.length)
+                        .map(i -> num_list[num_list.length - i])
+                        .toArray();
     }
 }

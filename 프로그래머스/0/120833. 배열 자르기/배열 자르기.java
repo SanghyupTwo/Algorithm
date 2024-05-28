8d9bbe9cfc99b5c1c6b793573e770549a4929bcf
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-05-28 10:24:28 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        int cnt = 0;
+        for(int i=num1; i<=num2; i++){
+            answer[cnt] = numbers[i];
+            cnt++;
+        }
+        return answer;
+    }
+}

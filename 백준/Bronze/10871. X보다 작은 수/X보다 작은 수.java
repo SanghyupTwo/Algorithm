4c227e1d09e302fef679ca0c80fa9b7d7fdb9071
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-06-20 15:52:06 +0900

    [Bronze V] Title: X보다 작은 수, Time: 432 ms, Memory: 22608 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -7,23 +7,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), x = sc.nextInt();
         sc.nextLine();
+        
         int[] arr = Arrays.stream(sc.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        int cnt = 0;
         for (int i : arr) {
             if (i < x) {
-                cnt++;
+                System.out.print(i + " ");
             }
-        }
-        int[] res = new int[cnt];
-        int tmp = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < x) res[tmp++] = arr[i];
-        }
-
-        for (int r : res) {
-            System.out.print(r + " ");
         }
     }
 }

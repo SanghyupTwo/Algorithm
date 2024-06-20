afb769d3dfffe612fa096239977565bebd8bc1a5
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-06-21 01:08:01 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 220 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -5,15 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i <= n; i++) {
-            // 공백
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-            // 별
-            for (int k = 0; k < n - i; k++) {
-                System.out.print("*");
-            }
+        for (int i = 0; i < n; i++) {
+            System.out.print(" ".repeat(i));
+            System.out.print("*".repeat(n-i));
             System.out.println();
         }
     }

f0257593179791a473f3b09f88ef8655d9c1d04e
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-06-20 18:05:15 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -7,10 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println("*".repeat(i));
         }
     }
 }

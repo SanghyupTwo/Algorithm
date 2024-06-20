7417ff63782a7d094ba1a65a9cd8ace196c7fa79
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-06-20 16:04:12 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+// 고양이
+public class Main{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}

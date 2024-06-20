1c73cceb91b5fad8eb7da8151f3c059edc6b56ec
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-06-20 15:57:33 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-class Main{
-    public static void main(String[] args){
+
+// A + B
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        
-        System.out.println(a+b);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(a + b);
     }
 }

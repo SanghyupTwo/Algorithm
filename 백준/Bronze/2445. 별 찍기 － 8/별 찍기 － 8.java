0eeca2d8b45a9f72e3100cc1d79b9919acfba8c6
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-06-21 14:37:53 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 248 ms, Memory: 17912 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -5,34 +5,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            // 별
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            // 공백 8->6->4->2->0
-            for (int k = 0; k < (2 * n) - (2 * i); k++) {
-                System.out.print(" ");
-            }
-            // 별
-            for (int l = 0; l < i; l++) {
-                System.out.print("*");
-            }
+        
+                for (int i = 1; i <= n; i++) {
+            //별
+            System.out.print("*".repeat(i));
+            //공백
+            System.out.print(" ".repeat((n * 2) - (2 * i)));
+            //별
+            System.out.print("*".repeat(i));
             System.out.println();
         }
+
         for (int i = 1; i < n; i++) {
-            // 별
-            for (int j = 0; j < n - i; j++) {
-                System.out.print("*");
-            }
-            // 공백 2->4->6
-            for (int k = 0; k < (2 * i); k++) {
-                System.out.print(" ");
-            }
-            // 별
-            for (int l = 0; l < n - i; l++) {
-                System.out.print("*");
-            }
+            //별
+            System.out.print("*".repeat(n - i));
+            //공백
+            System.out.print(" ".repeat(2 * i));
+            //별
+            System.out.print("*".repeat(n - i));
             System.out.println();
         }
     }

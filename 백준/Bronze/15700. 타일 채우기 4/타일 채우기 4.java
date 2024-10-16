189a83bea165f2e27be06163a95737ebebd1863d
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-10-16 12:38:44 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong(), m = sc.nextLong();
+        System.out.println(n * m / 2);
+    }
+}

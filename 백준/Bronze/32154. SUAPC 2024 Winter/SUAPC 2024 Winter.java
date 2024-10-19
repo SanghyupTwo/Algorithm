097e00454b4ea59f76dd65890f7061d5e92e4d0f
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2024-10-19 12:22:10 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 200 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        switch(n) {
+            case 1:
+                System.out.println("11");
+                System.out.println("A B C D E F G H J L M");
+                break;
+            case 2:
+                System.out.println("9");
+                System.out.println("A C E F G H I L M");
+                break;
+            case 3:
+                System.out.println("9");
+                System.out.println("A C E F G H I L M");
+                break;
+            case 4:
+                System.out.println("9");
+                System.out.println("A B C E F G H L M");
+                break;
+            case 5:
+                System.out.println("8");
+                System.out.println("A C E F G H L M");
+                break;
+            case 6:
+                System.out.println("8");
+                System.out.println("A C E F G H L M");
+                break;
+            case 7:
+                System.out.println("8");
+                System.out.println("A C E F G H L M");
+                break;
+            case 8:
+                System.out.println("8");
+                System.out.println("A C E F G H L M");
+                break;
+            case 9:
+                System.out.println("8");
+                System.out.println("A C E F G H L M");
+                break;
+            case 10:
+                System.out.println("8");
+                System.out.println("A B C F G H L M");
+                break;
+        }
+    }
+}

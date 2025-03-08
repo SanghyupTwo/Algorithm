caef357e13af965715e6e254e192c23419be6bf4
Sanghyup Lee <116155442+SanghyupTwo@users.noreply.github.com>
2025-03-08 20:07:22 +0900

    [Bronze V] Title: AI 선도대학, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n - 2024 );
+    }
+}
